506de42f447440407adf904850754dae156f82cc
Uliana Halushchak <30951313+UlianaHalushchak@users.noreply.github.com>
2017-10-02 01:20:39 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 
+var form = document.createElement('form')
+form.name = 'login';
+form.action = 'google.com';
+form.id = 'validateForm';
+
+var input1 = document.createElement('input');
+input1.type = 'text';
+input1.name = 'username';
+input1.placeholder = 'username';
+
+
+var input2 = document.createElement('input');
+input2.type = 'text';
+input2.name = 'date';
+input2.placeholder = 'DD.MM.YYYY';
+
+var input3 = document.createElement('input');
+input3.type = 'text';
+input3.name = 'age';
+input3.placeholder = 'age';
+
+var input4 = document.createElement('input');
+input4.type = 'submit';
+input4.value = 'Validate Me';
